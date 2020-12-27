61bc09fcd009380ad8d94b2a2e50293f87934b96
oranMeushar <oranmeushar@gmail.com>
2020-12-27 11:35:20 +0200

    omnifood done

---
+++ app.js
@@ -0,0 +1,35 @@
+document.addEventListener('scroll', ()=> {
+  
+    let top  = window.pageYOffset + window.innerHeight;
+    let  isPhoneVisible = top > document.querySelector('.main-steps').offsetTop;
+    let isCitiesVisible = top > document.querySelector('.main-locations').offsetTop;
+    let isServiceVisible = top > document.querySelector('.main-payment-wrapper').offsetTop;
+
+
+    if (isPhoneVisible) {
+        document.querySelector('.main-steps-details-left').classList.add('isVisible');
+    }
+    else{
+        document.querySelector('.main-steps-details-left').classList.remove('isVisible');
+    }
+
+    if (isCitiesVisible) {
+        document.querySelector('.main-locations').classList.add('isVisible');
+    }
+    else{
+        document.querySelector('.main-locations').classList.remove('isVisible');
+    }
+
+    if (isServiceVisible) {
+        [...document.querySelectorAll('.main-payment-service')]
+        .map((e)=>{
+            e.classList.add('isVisible');
+        })
+    }
+    else{
+        [...document.querySelectorAll('.main-payment-service')]
+        .map((e)=>{
+            e.classList.remove('isVisible');
+        })
+    }
+});
